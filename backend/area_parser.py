a5c3e1b768a48aafe60368cfbf5d7a32750ca9ee
allenjin123 <allenjin@umich.edu>
2025-10-29 14:35:55 -0400

    feat: updated ilp format

---
+++ backend/area_parser.py
@@ -0,0 +1,5 @@
+import argparse
+
+# call PdatScorrWrapper
+# create a DSL representation of the input instructions
+# example DSL representation: /home/allenjin/Codes/egraph_isa_compiler_codesign/PdatScorrWrapper/PdatDsl/workload/rv32im_no_add.dsl
---
+++ backend/backend_parser.py
@@ -0,0 +1,6 @@
+import argparse
+
+# parse user input arguments
+# - input_files: path to the input files
+# parse input_files, maintain a set structures for each file to analyze how many instructions are used
+# example input_files: /home/allenjin/Codes/egraph_isa_compiler_codesign/backend/test_inputs/rv32im_no_add.txt,/home/allenjin/Codes/egraph_isa_compiler_codesign/backend/test_inputs/rv32im_addi_lw_sw.txt
