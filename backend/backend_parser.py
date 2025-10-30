4adf8716c068f25784cdf9aab856b4cff6fa884e
allenjin123 <allenjin@umich.edu>
2025-10-30 15:38:37 -0400

    fix: fixed imm parsing bugs; removed addr type

--- backend/backend_parser.py
+++ backend/backend_parser.py
@@ -1,6 +1,43 @@
 import argparse
+import json
 
 # parse user input arguments
 # - input_files: path to the input files
 # parse input_files, maintain a set structures for each file to analyze how many instructions are used
-# example input_files: /home/allenjin/Codes/egraph_isa_compiler_codesign/backend/test_inputs/rv32im_no_add.txt,/home/allenjin/Codes/egraph_isa_compiler_codesign/backend/test_inputs/rv32im_addi_lw_sw.txt
+# example input_files: 
+# └── bitcnts_small_O3
+#     ├── variant_0
+#     │   ├── basic_blocks
+#     │       └── 0.txt
+#     │       └── 1.txt
+#     └── variant_1
+#         ├── basic_blocks
+#             └── 0.txt
+#             └── 1.txt
+
+import Saturation.data_structure as ds
+
+class program():
+    def __init__(self, name, path):
+        self.name = name
+        # path to the rewritten program directory
+        self.path = path 
+        # variants, a list of text program data structures
+        self.variants = []
+        # summary information for each variant
+        self.subsets = []
+        self.areas = []
+        self.delays = []
+
+    def parse_variants(self):
+        for i, variant in enumerate(self.path.iterdir()):
+            if variant.is_dir():
+                prog = ds.text_program(f"{self.name}_variant_{i}")
+                prog.from_directory(str(variant), suffix="_ssa")
+                self.variants.append(prog)
+
+    def parse_subsets(self):
+        for variant in self.variants:
+            subset = set()
+            
+
